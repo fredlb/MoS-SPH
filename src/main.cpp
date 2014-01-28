5673f1eae128d103613ce40c303b06130b1a568e
fredlb <fredrik.lindner@gmail.com>
2014-01-28 17:11:51 +0100

    gridzzz

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,57 @@
 #include "particles.h"
 #include "shader.h"
 
+#include <vector>
+#include <math.h>
+
 #define kParticlesCount 1000
-#define kWindowWidth 800
-#define kWindowHeight 600
-float kDt = 0.001f;
+#define kWindowWidth 640
+#define kWindowHeight 480
+#define kPi 3.14159265359
+
 
 void advance();
 void render();
 void glInit();
 void particlesInit();
 void drawGrid();
+void updateGrid();
 
 unsigned int vao;
 unsigned int vbo;
 unsigned int shader_programme;
 
+const float kViewScale =  2.0f;
+const float interactionRadius =  0.1f;
+const float cellSize = interactionRadius;
+const float kDt = 0.001f;
+const int kCellCount = 100;
+
+
+
+struct particle
+{
+	float m_x;
+	float m_y;
+	float m_u; //x-velocity
+	float m_v; //y-velocity
+
+	particle* next;
+};
+
+
+struct point
+{
+	float x;
+	float y;
+};
+
+typedef std::vector<particle> pVec;
 pVec pSys;
 
-// grid
-const float h = 0.1f;
+particle particles[kParticlesCount];
+particle* grid[kCellCount][kCellCount];
+
 
 int main () {
   // start GL context and O/S window using the GLFW helper library
@@ -54,7 +86,7 @@ int main () {
   const GLubyte* version = glGetString (GL_VERSION); // version as a string
   printf ("Renderer: %s\n", renderer);
   printf ("OpenGL version supported %s\n", version);
-  particlesInit();
+  //particlesInit();
   glInit();
 
   while (!glfwWindowShouldClose (window)) 
@@ -85,7 +117,7 @@ void advance()
 {
   for(int i = 0; i < kParticlesCount; i++)
   {
-    pSys[i].update(kDt);
+    //pSys[i].update(kDt);
 
   }
     //std::cout << "x: " << pSys[0].m_x << " y: " << pSys[0].m_y << " u: " << pSys[0].m_u << "  v: " << pSys[0].m_v << std::endl;
@@ -98,7 +130,7 @@ void render()
 	vbo = 0;
     glGenBuffers (1, &vbo);
     glBindBuffer (GL_ARRAY_BUFFER, vbo);
-    glBufferData (GL_ARRAY_BUFFER, kParticlesCount * sizeof(particle), &pSys[0], GL_STATIC_DRAW);
+    glBufferData (GL_ARRAY_BUFFER, kParticlesCount * sizeof(particle), &particles[0], GL_STATIC_DRAW);
 
     vao = 0;
     glGenVertexArrays (1, &vao);
@@ -136,23 +168,108 @@ void particlesInit()
   std::mt19937 eng((std::random_device())());
   std::uniform_real_distribution<> pos_dist(-1,1);
   std::uniform_real_distribution<> vel_dist(-0.3,0.3);
-
-  //give all particles random starting positions and velocities
-  for(int i = 0; i < kParticlesCount; i++)
-  {
-    float x = pos_dist(eng);
-    float y = pos_dist(eng);
-    float u = vel_dist(eng);
-    float v = vel_dist(eng);
-
-    particle p = particle(x,y,u,v);
-    pSys.push_back(p);
-    //std::cout << "x: " << p.m_x << " y: " << p.m_y << " u: " << p.m_u << "  v: " << p.m_v << std::endl;
-  }
-  
 }
 
-void drawGrid()
+void updateGrid()
+{
+	memset(grid, 0, kCellCount*sizeof(particle*));
+
+	for(size_t i = 0; i < kParticlesCount; i++)
+	{
+		particle& pi = particles[i];
+
+		int x = pi.m_x/cellSize;
+		int y = pi.m_y/cellSize;
+
+		if(x < 1)
+		{
+			x = 1;
+		}
+		else if( x > kCellCount - 2)
+		{
+			x = kCellCount - 2;
+		}
+
+		if(y < 1)
+		{
+			y = 1;
+		}
+		else if( y > kCellCount - 2)
+		{
+			y = kCellCount - 2;
+		}
+
+		pi.next = grid[x][y];
+		grid[x][y] = &pi;
+	}
+}
+
+
+void loopStructure()
+{
+	for(size_t i = 0; i < kParticlesCount; ++i)
+	{
+		particle& pi = particles[i];
+		int x = pi.m_x/cellSize;
+		int y = pi.m_y/cellSize;
+
+		//loop over cells 
+		for(size_t gx = x-1; gx < x+1; gx++)
+		{
+			for(size_t gy = y-1; gy < y+1; gy++)
+			{
+				//loop over neighbors
+				for (particle* ppj=grid[gx][gy]; NULL!=ppj; ppj=ppj->next)
+				{
+					//do fancy math
+				}
+			}
+		}
+	}
+}
+
+float WKernel(float distance2)
+{
+	return 0.0f;
+}
+
+float Wdeafult(float distance2)
 {
 
+	float W = (315/(64*kPi*pow(interactionRadius,9))) * pow((pow(interactionRadius,2) -distance2),3);
+	return W;
 }
+
+float[] WgradPressure(float dx, float dy)
+{
+	float distance2 = dx*dx + dy*dy;
+	float Wx = -(45/(kPi*pow(interactionRadius,6)))*(dx/sqrt(distance2))*pow((h-sqrt(distance2)),2);
+	float Wy = -(45/(kPi*pow(interactionRadius,6)))*(dx/sqrt(distance2))*pow((h-sqrt(distance2)),2);
+}
+
+float WlaplacianViscosity(float distance2)
+{
+	float W = (45/(kPi*pow(interactionRadius,6)))*(h-sqrt(distance2));
+	return W;
+}
+
+float WgradDefult(float dx, float dy)
+{
+	float distance2 = dx*dx + dy*dy;
+	float Wx = -(945/(32*kPi*pow(h,9)))*dx*pow((pow(interactionRadius,2)-distance2),2);
+	float Wy = -(945/(32*kPi*pow(h,9)))*dx*pow((pow(interactionRadius,2)-distance2),2);
+}
+
+float WlaplacianDefult(float distance2)
+{
+	float W = -(945/(32*kPi*pow(h,9)))*(pow(interactionRadius,2)-distance2)*(3*pow(interactionRadius,2)-7*distance2);
+	return W;
+}
+
+float distance2(particle* currentParticle, particle* neighbour)
+{
+	// |ri - rj|^2
+	return (currentParticle->m_x - neighbour->m_x)*(currentParticle->m_x - neighbour->m_x) +
+		(currentParticle->m_y - neighbour->m_y)*(currentParticle->m_y - neighbour->m_y);
+}
+
